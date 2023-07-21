1a103a48940103bc129ca829cd243dcb191548bd
Kimutai <jankimsingoei@gmail.com>
2023-07-21 19:34:29 +0300

    COMPLETED THE ASSIGNMENT

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit',(e) => {
+  e.preventDefault();
+  let input = document.getElementById('new-task-description').value
+  let li = document.createElement('li')
+  li.textContent= input
+  document.querySelector('#tasks').appendChild(li)
+  let deleteButton= document.createElement('button')
+  li.appendChild(deleteButton)
+  deleteButton.textContent = "x"
+  document.querySelector('form').reset()
+  function addEventListenerToButton(){
+    deleteButton.addEventListener('click', function(e){
+      e.target.parentNode.remove()
+    })
+    
+  }
+  addEventListenerToButton() 
+})
+})
+
+
+  
+  
+
+
+
+
